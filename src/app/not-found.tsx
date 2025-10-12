ec2ff0215087ea3049381216eb80fc61637ff906
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-10-12 14:04:40 -0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex min-h-screen items-center justify-center px-6">
+    <main className="flex min-h-[85vh] items-center justify-center px-6">
       <div className="text-center">
         <h1 className="font-sf text-6xl font-semibold">Err 404</h1>
 
         <p className="font-wdxl text-2xl font-medium">Page not found</p>
         <Link href="/">
           <Button variant="fancy-outline" className="mt-2 px-4 py-1.5">
-            home
+            Home
           </Button>
         </Link>
       </div>
