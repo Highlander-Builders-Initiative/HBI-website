0e53622b62478349daa22517f7e3e4d9395d9517
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-10-12 00:09:01 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-6">
       <div className="text-center">
-        <h1 className="font-sf font-semibold text-5xl md:text-6xl">
+        <h1 className="font-sf text-5xl font-semibold md:text-6xl">
           HIGHLANDER BUILDERS INITIATIVE
         </h1>
-        <p className="font-wdxl pt-2 sm:pt-0 text-2xl">
-          COMING WINTER 2025
-        </p>
+        <p className="font-wdxl pt-2 text-2xl sm:pt-0">COMING WINTER 2025</p>
       </div>
     </main>
   );
