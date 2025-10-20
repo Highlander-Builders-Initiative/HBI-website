c3ee4e690911994b1a22f77846478d1e679a80fe
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-10-19 22:35:21 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,6 +62,14 @@ export default function Home() {
           </Link>
           <span className="text-neutral-300">/</span>
           <Link
+            href="https://discord.gg/QYCQwTTvfS"
+            target="_blank"
+            className="font-instrument text-neutral-700 decoration-neutral-300 decoration-2 hover:text-black hover:underline"
+          >
+            discord
+          </Link>
+          <span className="text-neutral-300">/</span>
+          <Link
             href="mailto:contact@highlander.builders"
             className="font-instrument text-neutral-700 decoration-neutral-300 decoration-2 hover:text-black hover:underline"
           >
