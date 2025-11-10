7e2e01baa8d3248ecea588cf26003a4e8c852c1c
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-11-10 12:08:56 -0800

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -11,12 +11,23 @@ export default function Team() {
         text="THE BOARD"
       />
 
-      <p className="font-sf mt-6 text-lg">
-        Our founding team is what made this organization possible.
-      </p>
-      <p className="font-sf -mt-0.5 text-lg">
-        We&apos;re here to help you on your journey on building.
-      </p>
+      <div className="hidden sm:block">
+        <p className="font-sf mt-6 text-lg">
+          Our founding team is what made this organization possible.
+        </p>
+        <p className="font-sf -mt-0.5 text-lg">
+          We&apos;re here to help you on your building journey.
+        </p>
+      </div>
+
+      <div className="block sm:hidden">
+        <p className="font-sf mt-6 text-center text-lg">
+          This is our founding team.
+        </p>
+        <p className="font-sf -mt-0.5 text-lg">
+          We&apos;re here to support your journey.
+        </p>
+      </div>
     </div>
   );
 }
